d642b2e03f36bb597ca9b9da8ebd1d8def71166c
ossan-engineer <bodega1123@gmail.com>
2025-12-26 19:26:18 +0900

    Add utility types and object exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Object / 構造制御
+export type { StrictOmit, XOR } from './object'
+
+// Utility / 制約表現
+export type { RequireAtLeastOne, RequireOnlyOne, NonEmptyArray, DeepReadonly } from './utility'
---
+++ src/utility/index.ts
@@ -0,0 +1,4 @@
+export type { RequireAtLeastOne } from './require-at-least-one'
+export type { RequireOnlyOne } from './require-only-one'
+export type { NonEmptyArray } from './non-empty-array'
+export type { DeepReadonly } from './deep-readonly'
